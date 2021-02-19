cda3876b37eb0b11df1842f7373b6f8ba08e4c3c
obmeir <orenbm11@gmail.com>
2021-02-19 15:53:59 -0500

    initial commit

---
+++ Tasktower.OcelotAPIGateway/Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Tasktower.OcelotAPIGateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(
+                    ic => ic.AddJsonFile(
+                        Path.Combine("Configuration","Configuration.json")))
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
