54b32ebac6012a97cc5a2422ea1e22e8d8786fb9
Oren Ben-Meir <orenbm11@gmail.com>
2021-05-30 01:51:19 -0400

    login form with csrf

--- Tasktower.OcelotGateway/Controllers/AuthController.cs
+++ Tasktower.OcelotGateway/Controllers/AuthController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Globalization;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Antiforgery;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Tasktower.OcelotGateway.Configuration.StartupExtensions;
 using Tasktower.OcelotGateway.Security;
 
 namespace Tasktower.OcelotGateway.Controllers
@@ -13,16 +15,30 @@ namespace Tasktower.OcelotGateway.Controllers
     [Route("client/auth")]
     public class AuthController : ControllerBase
     {
-        [HttpPost("login")]
-        public async Task Login([FromQuery]string returnUrl)
+        
+        private readonly IAntiforgery _antiForgery;
+        
+        public AuthController(IAntiforgery antiForgery)
         {
+            _antiForgery = antiForgery;
+        }
+        
+        [IgnoreAntiforgeryToken]
+        [HttpPost("login")]
+        public async Task Login([FromQuery]string returnUrl, [FromForm] string xsrfToken)
+        {
+            HttpContext.Request.Headers[SecurityConfig.XsrfTokenHeaderName] = xsrfToken;
+            await _antiForgery.ValidateRequestAsync(HttpContext);
             await HttpContext.ChallengeAsync("Auth0", new AuthenticationProperties() { RedirectUri = returnUrl });
         }
         
+        [IgnoreAntiforgeryToken]
         [Authorize]
         [HttpPost("logout")]
-        public async Task Logout([FromQuery]string returnUrl)
+        public async Task Logout([FromQuery]string returnUrl, [FromForm] string xsrfToken)
         {
+            HttpContext.Request.Headers[SecurityConfig.XsrfTokenHeaderName] = xsrfToken;
+            await _antiForgery.ValidateRequestAsync(HttpContext);
             await HttpContext.SignOutAsync("Auth0", new AuthenticationProperties
             {
                 // Indicate here where Auth0 should redirect the user after a logout.
